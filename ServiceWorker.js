bb053fb64bdd52855ddcff64b46994f0a4468e68
QuatiConde <victormanoelxzk@hotmail.com>
2025-11-24 00:19:37 -0300

    Primeir commit

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "AruanaEstudio-MiniGames-1.0";
+const contentToCache = [
+    "Build/WebApp MiniGames.loader.js",
+    "Build/WebApp MiniGames.framework.js",
+    "Build/WebApp MiniGames.data",
+    "Build/WebApp MiniGames.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
