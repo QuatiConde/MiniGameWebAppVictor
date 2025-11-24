d7cf9727e5b36e7d626bbdb4e375a92836a85e7e
QuatiConde <victormanoelxzk@hotmail.com>
2025-11-24 13:47:54 -0300

    mudei a cor bora ver se atualiza.

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,7 +2,7 @@ const cacheName = "AruanaEstudio-MiniGames-1.0";
 const contentToCache = [
     "Build/034c16847400809a51c82ea76604f8ef.loader.js",
     "Build/61100263d9916275ebf3d1908c158bb5.framework.js",
-    "Build/124cd3a4aad29467931ac27a936762b9.data",
+    "Build/867142be845a360f6c4760f41e67eaf6.data",
     "Build/ffb90e3ce3b984fa668aff17e1ef93bb.wasm",
     "TemplateData/style.css"
 
