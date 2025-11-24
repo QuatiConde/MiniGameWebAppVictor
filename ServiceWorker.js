fa5de8d77da2251c2cabaa928ed6b4959fbe937e
QuatiConde <victormanoelxzk@hotmail.com>
2025-11-24 00:36:47 -0300

    commit 3 tentei uma alternativa
    bora ver se  o cache se ataulzia

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "AruanaEstudio-MiniGames-1.0";
 const contentToCache = [
-    "Build/MiniGameWebAppVictor.loader.js",
-    "Build/MiniGameWebAppVictor.framework.js",
-    "Build/MiniGameWebAppVictor.data",
-    "Build/MiniGameWebAppVictor.wasm",
+    "Build/50fe56193d243dfcea4e4148f8eb5357.loader.js",
+    "Build/5fa4674293210e1991f761a8e58fb268.framework.js",
+    "Build/2394ac13ceb26814d47c0fa059088ad3.data",
+    "Build/2a3ba7e5ca042bd626e989aacaa979ef.wasm",
     "TemplateData/style.css"
 
 ];
