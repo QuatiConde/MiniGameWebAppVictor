07d74108f41686dd7994df0fcf7d913a1caa8655
QuatiConde <victormanoelxzk@hotmail.com>
2025-11-24 12:01:09 -0300

    Commit do unity com botão para teste

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "AruanaEstudio-MiniGames-1.0";
 const contentToCache = [
-    "Build/50fe56193d243dfcea4e4148f8eb5357.loader.js",
-    "Build/5fa4674293210e1991f761a8e58fb268.framework.js",
-    "Build/2394ac13ceb26814d47c0fa059088ad3.data",
-    "Build/2a3ba7e5ca042bd626e989aacaa979ef.wasm",
+    "Build/f6a881a0e80cb02d40c8b23c84272ec3.loader.js",
+    "Build/61100263d9916275ebf3d1908c158bb5.framework.js",
+    "Build/9710ac7ca42e99906ddcc2d4a488c637.data",
+    "Build/5ef08b5ae7a0e91e95c4a791bf054546.wasm",
     "TemplateData/style.css"
 
 ];
