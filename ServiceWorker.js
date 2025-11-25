23ad3c4bb5afd118b7a4ec72f896d5782db2d57d
QuatiConde <victormanoelxzk@hotmail.com>
2025-11-25 07:00:49 -0300

    teste 5 passei 2 horas

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,7 +2,7 @@ const cacheName = "AruanaEstudio-MiniGames-1.0";
 const contentToCache = [
     "Build/6ffa0106efff3cfc2f7ae80ab5987d4f.loader.js",
     "Build/d94632cd5f22098d52d5eb7a5ec7a057.framework.js",
-    "Build/b3ee43083333a40b309bfe327c9ca011.data",
+    "Build/051b789eb284fccfd0bbfd08bad23c52.data",
     "Build/e709c1ba04c3257fad418fb2f2da5a4b.wasm",
     "TemplateData/style.css"
 
