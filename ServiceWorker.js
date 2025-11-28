b2044722a3db9da0d16b5a11ba3d5a5d633c3fde
QuatiConde <victormanoelxzk@hotmail.com>
2025-11-28 07:34:47 -0300

    att10

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,8 +2,8 @@ const cacheName = "AruanaEstudio-MiniGames-1.0";
 const contentToCache = [
     "Build/340603054bfe50d205948d123ba28627.loader.js",
     "Build/510fbbb40c26faf67227cc8abe963ca4.framework.js",
-    "Build/362e7b609d4fc6f02f55b70ff4dddb32.data",
-    "Build/96a34562188b71b6a633d732cea7e6ff.wasm",
+    "Build/93d57152deab5e1078a435f00279ee6d.data",
+    "Build/037148e4f66b79ccd264dce5f0ce97c8.wasm",
     "TemplateData/style.css"
 
 ];
